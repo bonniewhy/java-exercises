c9a4a89e51c17ae2d5933a6f8a3b48f6ab034bac
Bonnie Why <bonnieinpixels@gmail.com>
2019-01-03 15:57:50 -0600

    Adding more work for exercise reviews

---
+++ src/org/launchcode/java/exercises/school/Student.java
@@ -0,0 +1,54 @@
+package org.launchcode.java.exercises.school;
+
+public class Student {
+
+    private static int nextStudentId = 1;
+    private String name;
+    private final int studentId;
+    private int numberOfCredits;
+    private double gpa;
+
+    public Student(String name, int studentId, int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public Student(String name, int studentId) {
+        this(name, studentId, 0, 0);
+    }
+
+    public Student(String name) {
+        this(name, nextStudentId);
+        nextStudentId++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
+
+    public void setNumberOfCredits(int nextNumberofCredits) {
+        numberOfCredits = nextNumberofCredits;
+    }
+
+    public void setGpa(double nextGpa) {
+        gpa = nextGpa;
+    }
+}
